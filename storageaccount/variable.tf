a7830538758156ae56a1fc523f5c0afca98cfc59
Atisha Shaurya <94087803+Atisha-Shaurya@users.noreply.github.com>
2023-03-02 17:28:01 +0530

    Update variable.tf

--- storageaccount/variable.tf
+++ storageaccount/variable.tf
@@ -1,17 +1,19 @@
 variable "resourcename" {
+  type = string
   description = "this is resourcegroup"
 }
 variable "location" {
+  type = string
 }
-
 variable "storagename" {
+  type = string
 }
 variable "tags" {
   type = map(any)
 }
 variable "containername" {
+  type = string
 }
 variable "blobstorage" {
+  type = string
 }
-
-
